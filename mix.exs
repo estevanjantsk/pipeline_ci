76e13dc3f8921351e436d8e850fe98386055a662
Estevan Jantsk <estevan.jantsk@gmail.com>
2024-03-07 22:23:14 +0200

    add dialyxir

--- mix.exs
+++ mix.exs
@@ -60,6 +60,7 @@ defmodule PipelineCi.MixProject do
       {:bandit, "~> 1.2"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.18", only: :test},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
     ]
   end
 
